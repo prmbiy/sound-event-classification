6945a0d933c51eeab1b0af84550c5fe0099019f7
Soham Tiwari <soham.tiwari800@gmail.com>
2022-01-10 02:39:21 +0530

    modified statistics.py to accomodate train or validate folders

--- dcase/statistics.py
+++ dcase/statistics.py
@@ -1,10 +1,10 @@
-import numpy as np 
+import numpy as np
 from tqdm import tqdm
-import pickle 
+import pickle
 from glob import glob
 import argparse
 
-def run(feature_type, num_bins):
+def run(feature_type, num_bins, data_type):
 
     # Load and prepare data
     with open('./metadata/metadata.pkl', 'rb') as f:
@@ -12,13 +12,13 @@ def run(feature_type, num_bins):
 
     train_df = metadata['coarse_train']
     file_list = list(set(train_df.index.tolist()))
-    
+
     mean = np.zeros((num_bins,))
     M2 = np.zeros((num_bins,))
 
     n = 0
     for file_name in tqdm(file_list):
-        data = np.load('./data/{}/{}.npy'.format(feature_type, file_name))
+        data = np.load('./data/{}/{}/{}.npy'.format(feature_type, data_type, file_name))
         x = data.mean(axis=1)
         n += 1
         delta = x - mean
@@ -32,10 +32,11 @@ def run(feature_type, num_bins):
     np.save('./data/statistics/channel_stds_{}'.format(feature_type), stdev)
 
 if __name__=="__main__":
-    
+
     parser = argparse.ArgumentParser(description='Feature type')
     parser.add_argument('-f', '--feature_type', type=str, default='logmelspec')
     parser.add_argument('-n', '--num_bins', type=int, default=128)
+    parser.add_argument('-t', '--data_type', type=str, default='train')
     args = parser.parse_args()
-    run(args.feature_type, args.num_bins)
+    run(args.feature_type, args.num_bins, args.data_type)
 
