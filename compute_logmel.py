694922550cbf19e8228c27f742a7d3e7749416ac
Soham Tiwari <soham.tiwari800@gmail.com>
2022-03-17 00:13:45 +0530

    fix: added logging to compute logmel, and updated config params to change according to sr

--- compute_logmel.py
+++ compute_logmel.py
@@ -1,14 +1,15 @@
 import os
-import sys
 import librosa
 import numpy as np
-import pandas as pd
 from joblib import Parallel, delayed
 from glob import glob
 from tqdm import tqdm
 import argparse
+from loguru import logger
 from config_global import n_fft, hop_length, n_mels, fmin, fmax, sample_rate, num_cores, remove_codec_from_filename
 
+
+@logger.catch
 def remove_codec_substr(filename: str, remove_codec_from_filename: bool = remove_codec_from_filename):
     """Utility function to remove codec substring from audio files in audioset dataset.
 
@@ -18,17 +19,19 @@ def remove_codec_substr(filename: str, remove_codec_from_filename: bool = remove
 
     Returns:
         str: Final filepath to be used.
-    """    
+    """
     output_filename = os.path.basename(filename)
     if remove_codec_from_filename:
         output_filename = output_filename[:output_filename.rindex('_')]+'.wav'
     return output_filename
 
+
+@logger.catch
 def compute_melspec(filename, outdir, audio_segment_length):
     try:
         wav = librosa.load(filename, sr=sample_rate)[0]
-        if(audio_segment_length!=-1 and audio_segment_length!=0):
-            wav = wav [:sample_rate*audio_segment_length]
+        if(audio_segment_length != -1 and audio_segment_length != 0):
+            wav = wav[:sample_rate*audio_segment_length]
         melspec = librosa.feature.melspectrogram(
             wav,
             sr=sample_rate,
@@ -38,23 +41,43 @@ def compute_melspec(filename, outdir, audio_segment_length):
             fmin=fmin,
             fmax=fmax)
         logmel = librosa.core.power_to_db(melspec)
-        
-        np.save(outdir + remove_codec_substr(filename, remove_codec_from_filename) + '.npy', logmel)
+
+        np.save(outdir + remove_codec_substr(filename,
+                remove_codec_from_filename) + '.npy', logmel)
     except ValueError:
         print('ERROR IN:', filename)
 
+
+@logger.catch
 def main(input_path, output_path, audio_segment_length):
+    logger.add(f'compute_logmel_sr={sample_rate}.log', rotation='100 KB')
+    logger.info(f"PARAMS:")
+    logger.info(f"n_fft = {n_fft}")
+    logger.info(f"hop_length = {hop_length}")
+    logger.info(f"n_mels = {n_mels}")
+    logger.info(f"fmin = {fmin}")
+    logger.info(f"fmax = {fmax}")
+    logger.info(f"sample_rate = {sample_rate}")
+    logger.info(f"num_cores = {num_cores}")
+    logger.info(f"remove_codec_from_filename = {remove_codec_from_filename}")
+    logger.info(f'Starting computing logmels using above params.')
     file_list = glob(input_path + '/*.wav')
     os.makedirs(output_path, exist_ok=True)
     _ = Parallel(n_jobs=num_cores)(
-            delayed(lambda x: compute_melspec(x, output_path + '/', audio_segment_length))(x)
-            for x in tqdm(file_list))
+        delayed(lambda x: compute_melspec(
+            x, output_path + '/', audio_segment_length))(x)
+        for x in tqdm(file_list))
+    logger.success(f'Finished computing logmels using sr = {sample_rate}')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Input and Output Paths')
-    parser.add_argument('input_path', type=str, help="Specifies directory of audio files.")
-    parser.add_argument('output_path', type=str, help="Specifies directory for generated spectrograms.")
-    parser.add_argument('-a', '--audio_segment_length', type=int, help="Specifies length of audio segment to extract from each audio file. Default -1(Consider full length audio).", default=-1)
+    parser.add_argument('input_path', type=str,
+                        help="Specifies directory of audio files.")
+    parser.add_argument('output_path', type=str,
+                        help="Specifies directory for generated spectrograms.")
+    parser.add_argument('-a', '--audio_segment_length', type=int,
+                        help="Specifies length of audio segment to extract from each audio file. Default -1(Consider full length audio).", default=-1)
     args = parser.parse_args()
 
     main(args.input_path, args.output_path, args.audio_segment_length)
--- config_global.py
+++ config_global.py
@@ -1,9 +1,9 @@
 # logmel spectrogram values
-n_fft = 2560
-hop_length = 694
+sample_rate = 16000
+n_fft=(2560*sample_rate)//44100
+hop_length=(694*sample_rate)//44100
 n_mels = 128
 fmin = 20
-sample_rate = 16000
 remove_codec_from_filename = True
 fmax = sample_rate/2
 num_cores = -1 # -1 implies take all available cores.
