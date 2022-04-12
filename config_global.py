1d78d9d0973a42d293616d9463a032afbe814c30
Soham Tiwari <soham.tiwari800@gmail.com>
2022-04-13 01:41:16 +0530

    fix: trying inverse approach for n_fft and hop_len

--- config_global.py
+++ config_global.py
@@ -1,7 +1,10 @@
 # logmel spectrogram values
+from random import sample
+
+
 sample_rate = 16000
-n_fft=(2560*sample_rate)//44100
-hop_length=(694*sample_rate)//44100
+n_fft=(2560*44100)//sample_rate
+hop_length=(694*44100)//sample_rate
 n_mels = 128
 fmin = 20
 remove_codec_from_filename = True
