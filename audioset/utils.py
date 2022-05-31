a184a20c3c2e17676e32af226d1ed90aff2277d9
Soham Tiwari <soham.tiwari800@gmail.com>
2022-05-31 14:16:32 +0530

    fix: added missing attention2d class

--- audioset/utils.py
+++ audioset/utils.py
@@ -70,6 +70,62 @@ def getLabelFromFilename(file_name: str) -> int:
     return label
 
 
+class attention2d(nn.Module):
+    def __init__(self, in_planes, kernel_size, stride, padding, n_basis_kernels, temperature, pool_dim):
+        super(attention2d, self).__init__()
+        self.pool_dim = pool_dim
+        self.temperature = temperature
+
+        hidden_planes = int(in_planes / 4)
+
+        if hidden_planes < 4:
+            hidden_planes = 4
+
+        if not pool_dim == 'both':
+            self.conv1d1 = nn.Conv1d(
+                in_planes, hidden_planes, kernel_size, stride=stride, padding=padding, bias=False)
+            self.bn = nn.BatchNorm1d(hidden_planes)
+            self.relu = nn.ReLU(inplace=True)
+            self.conv1d2 = nn.Conv1d(
+                hidden_planes, n_basis_kernels, 1, bias=True)
+            for m in self.modules():
+                if isinstance(m, nn.Conv1d):
+                    nn.init.kaiming_normal_(
+                        m.weight, mode='fan_out', nonlinearity='relu')
+                    if m.bias is not None:
+                        nn.init.constant_(m.bias, 0)
+                if isinstance(m, nn.BatchNorm1d):
+                    nn.init.constant_(m.weight, 1)
+                    nn.init.constant_(m.bias, 0)
+        else:
+            self.fc1 = nn.Linear(in_planes, hidden_planes)
+            self.relu = nn.ReLU(inplace=True)
+            self.fc2 = nn.Linear(hidden_planes, n_basis_kernels)
+
+    def forward(self, x):  # x size : [bs, chan, frames, freqs]
+        if self.pool_dim == 'freq':
+            x = torch.mean(x, dim=3)  # x size : [bs, chan, frames]
+        elif self.pool_dim == 'time':
+            x = torch.mean(x, dim=2)  # x size : [bs, chan, freqs]
+        elif self.pool_dim == 'both':
+            # x = torch.mean(torch.mean(x, dim=2), dim=1)  #x size : [bs, chan]
+            x = F.adaptive_avg_pool2d(x, (1, 1)).squeeze(-1).squeeze(-1)
+        elif self.pool_dim == 'chan':
+            x = torch.mean(x, dim=1)  # x size : [bs, freqs, frames]
+
+        if not self.pool_dim == 'both':
+            x = self.conv1d1(x)  # x size : [bs, hid_chan, frames]
+            x = self.bn(x)
+            x = self.relu(x)
+            x = self.conv1d2(x)  # x size : [bs, n_ker, frames]
+        else:
+            x = self.fc1(x)  # x size : [bs, hid_chan]
+            x = self.relu(x)
+            x = self.fc2(x)  # x size : [bs, n_ker]
+
+        return F.softmax(x / self.temperature, 1)
+
+
 class Dynamic_conv2d(nn.Module):
     """To perform Frequency Dynamic Convolution or Time Dynamic Convolution.
     """
@@ -80,7 +136,8 @@ class Dynamic_conv2d(nn.Module):
 
         self.in_planes = in_planes
         self.out_planes = out_planes
-        self.kernel_size = kernel_size if isinstance(kernel_size, int) else kernel_size[0]
+        self.kernel_size = kernel_size if isinstance(
+            kernel_size, int) else kernel_size[0]
         self.groups = groups
         self.stride = stride
         self.padding = padding
