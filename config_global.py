03c63a1b797f80ab233261346aaaec7bfb8b2c2d
Soham Tiwari <soham.tiwari800@gmail.com>
2022-07-28 07:07:00 +0530

    fix: changed sr to 16k config global

--- config_global.py
+++ config_global.py
@@ -1,9 +1,9 @@
 # logmel spectrogram values
-sample_rate = 44100
+sample_rate = 16000
 n_fft=(2560*sample_rate)//44100
 hop_length=(694*sample_rate)//44100
 n_mels = 128
 fmin = 20
 remove_codec_from_filename = True
-fmax = 22050
+fmax = 8000
 num_cores = -1 # -1 implies take all available cores.
